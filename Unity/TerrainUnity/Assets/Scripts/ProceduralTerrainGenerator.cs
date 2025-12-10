e999dc41487f072b6f13642cbd04de7914528417
AstraaaV <150567554+AstraaaV@users.noreply.github.com>
2025-12-10 15:34:37 +0000

    Added octaves etc
    Added octaves, persistence, lacunarity, seeds, offset, and FBM (fractal brownian motion)

--- Unity/TerrainUnity/Assets/Scripts/ProceduralTerrainGenerator.cs
+++ Unity/TerrainUnity/Assets/Scripts/ProceduralTerrainGenerator.cs
@@ -5,9 +5,15 @@ public class ProceduralTerrainGenerator : MonoBehaviour
 {
     public int width = 513;
     public int height = 513;
+    public float heightMultiplier = 20f;
 
     public float noiseScale = 50f;
-    public float heightMultiplier = 20f;
+    public int octaves = 4;
+    public float persistence = 0.5f;
+    public float lacunarity = 2f;
+
+    public int seed = 0;
+    public Vector2 offset;
 
     Terrain terrain;
 
@@ -21,19 +27,34 @@ public class ProceduralTerrainGenerator : MonoBehaviour
     {
         float[,] heights = new float[width, height];
 
-        for(int x = 0; x < width; x++)
+        System.Random rnd = new System.Random();
+        float offsetX = offset.x + rnd.Next(-100000, 100000);
+        float offsetY = offset.y + rnd.Next(-100000, 100000);
+
+        for (int x = 0; x < width; x++)
         {
-            for(int y = 0; y < height; y++)
+            for (int y = 0; y < height; y++)
             {
-                float xCoord = (float)x / noiseScale;
-                float yCoord = (float)y / noiseScale;
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
 
-                float noiseValue = Mathf.PerlinNoise(xCoord, yCoord);
+                for (int i = 0; i < octaves; i++)
+                {
+                    float xCoord = (x / noiseScale * frequency) + offsetX;
+                    float yCoord = (y / noiseScale * frequency) + offsetY;
 
-                heights[x, y] = noiseValue * heightMultiplier / 100f;
+                    float perlinValue = Mathf.PerlinNoise(xCoord, yCoord);
+
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistence;
+                    frequency *= lacunarity;
+                }
+                heights[x, y] = noiseHeight * heightMultiplier / 100f;
+
             }
         }
-
         terrain.terrainData.heightmapResolution = width;
         terrain.terrainData.SetHeights(0, 0, heights);
 
